efed874a3f79fc505333a0394d64dad7a3e933f9
as-shamrat <shahriar4499@gmail.com>
2026-02-21 20:10:38 +0600

    cart slice done

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      console.log(action.payload)
+      const existingItem = state.items.find(item => item.name === action.payload.name);
+      if (existingItem) {
+        existingItem.quantity += 1; // Increment quantity if item already exists in the cart  
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 }); // Add new item to the cart with quantity 1
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
 
-    
+      const { name, quantity } = action.payload
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity = quantity; // Update the quantity of the item in the cart
+      }
     },
   },
 });
